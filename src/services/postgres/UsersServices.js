1d66ee6a12b05a3079fc38e5310dc4558b08e17d
elSyarif <id.syarif@gmail.com>
2022-12-21 16:50:31 +0700

    add: implemented user service & install bcrypt
    add function add userm get user by id, verfy user

--- src/services/postgres/UsersServices.js
+++ src/services/postgres/UsersServices.js
@@ -1,9 +1,32 @@
 const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const bcrypt = require('bcrypt');
 
 class UsersService {
   constructor() {
     this._pool = new Pool();
   }
+
+  async addUser({ fullname, username, password }) {
+    const id = `user-${nanoid(16)}`;
+    const hashPassword = await bcrypt.hash(password, 10);
+    const createdAt = new Date().toISOString();
+
+    const query = {
+      text: 'INSERT INTO users VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
+      values: [id, fullname, username, hashPassword, createdAt, createdAt],
+    };
+
+    const result = await this._pool.query(query);
+
+    return result.rows[0].id;
+  }
+
+  async getUserById(userId) {}
+  
+  async verifyUsername(username) {}
+
+  async verifyUserCriential(username, password) {}
 }
 
 module.exports = UsersService;
